912c949f7f12e46b0576f9d58428239bfbb71db4
Serhii-Kvach <chilim452@gmail.com>
2025-03-24 14:11:04 +0100

    edit

--- src/components/NoData/NoData.jsx
+++ src/components/NoData/NoData.jsx
@@ -1,10 +1,8 @@
-import { PiFilmSlateLight } from 'react-icons/pi'; // Іконка
-import css from './NoData.module.css';
+import css from "./NoData.module.css";
 
 export default function NoData() {
   return (
     <div className={css.container}>
-      <PiFilmSlateLight className={css.icon} />
       <h2 className={css.text}>No data available</h2>
       <p className={css.subtext}>Try searching for something else...</p>
     </div>
